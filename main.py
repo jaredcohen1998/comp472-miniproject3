de9ed5bb51150e5097e23ee8eb941ecaa1aa1504
jaforte <forte_j@hotmail.com>
2021-12-12 14:52:58 -0500

    Changed camel-case variable names to joined-lower case. Streamlined the syntax a litte.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from gensim.similarities import MatrixSimilarity
 import pandas as pd
 import random
 
-def experiment_with_model(modelName, synonymFile):
-    targetFile = F"data/{modelName}-model.csv"
 
-    print(F"\n\n===LOADING MODEL {modelName}===")
-    model = api.load(modelName)
+def experiment_with_model(model_name, synonym_file):
+    target_file = F"data/{model_name}-model.csv"
 
-    print(F"Loading {synonymFile}...")
+    print(F"\n\n===LOADING MODEL {model_name}===")
+    model = api.load(model_name)
+
+    print(F"Loading {synonym_file}...")
 
     question_word_list = []
     answer_word_list = []
@@ -18,9 +19,9 @@ def experiment_with_model(modelName, synonymFile):
     num_words = 0
     num_correct = 0
     num_guesses = 0
-    csv = pd.read_csv(synonymFile)
+    csv = pd.read_csv(synonym_file)
 
-    print(F"Gathering data to write to {targetFile}...")
+    print(F"Gathering data to write to {target_file}...")
 
     for row in csv.iterrows():
         r = row[1]
@@ -33,20 +34,20 @@ def experiment_with_model(modelName, synonymFile):
         w2_exists = r['1'] in model.key_to_index
         w3_exists = r['2'] in model.key_to_index
         w4_exists = r['3'] in model.key_to_index
-        words_exists = w1_exists == True or w2_exists == True or w3_exists == True or w4_exists == True
+        words_exists = w1_exists or w2_exists or w3_exists or w4_exists
 
-        if (keyword_exists == True):
-            if (w1_exists == True):
+        if (keyword_exists):
+            if (w1_exists):
                 cs_list.append(model.similarity(r['question'], r['0']))
-            if (w2_exists == True):
+            if (w2_exists):
                 cs_list.append(model.similarity(r['question'], r['1']))
-            if (w3_exists == True):
+            if (w3_exists):
                 cs_list.append(model.similarity(r['question'], r['2']))
-            if (w4_exists == True):
+            if (w4_exists):
                 cs_list.append(model.similarity(r['question'], r['3']))
 
         num_words = num_words + 1
-        if (keyword_exists == False or words_exists == False):
+        if (not (keyword_exists and words_exists)):
             label_list.append("guess")
             guess_word = random.choice([r['0'], r['1'], r['2'], r['3']])
             guess_word_list.append(guess_word)
@@ -64,35 +65,37 @@ def experiment_with_model(modelName, synonymFile):
             else:
                 label_list.append("wrong")
 
-    df = pd.DataFrame({"question": question_word_list, "answer": answer_word_list, "system guess word": guess_word_list, "label": label_list})
-    df.to_csv(targetFile, index=False)
+    df = pd.DataFrame({"question": question_word_list, "answer": answer_word_list,
+                      "system guess word": guess_word_list, "label": label_list})
+    df.to_csv(target_file, index=False)
 
-    print(F"Wrote to file {targetFile}")
+    print(F"Wrote to file {target_file}")
 
     if (num_words - num_guesses == 0):
         accuracy = 0
     else:
         accuracy = num_correct / (num_words - num_guesses)
 
-    return (modelName, model.vectors.shape[0], num_correct, num_words, num_guesses, accuracy)
+    return (model_name, model.vectors.shape[0], num_correct, num_words, num_guesses, accuracy)
 
 
 def main():
-    synonymFile = "data/synonyms.csv"
+    synonym_file = "data/synonyms.csv"
     models_list = [
         "word2vec-google-news-300", "glove-wiki-gigaword-300", "fasttext-wiki-news-subwords-300", "glove-twitter-50", "glove-twitter-100"
     ]
 
-    modelName_list = []
+    model_name_list = []
     model_size_list = []
     num_correct_list = []
     num_answered_list = []
     accuracy_list = []
 
-    for i in range(len(models_list)):
-        model_name, model_size, num_correct, num_words, num_guesses, accuracy = experiment_with_model(models_list[i], synonymFile)
+    for model in models_list:
+        model_name, model_size, num_correct, num_words, num_guesses, accuracy = experiment_with_model(
+            model, synonym_file)
 
-        modelName_list.append(model_name)
+        model_name_list.append(model_name)
         model_size_list.append(model_size)
         num_correct_list.append(num_correct)
         num_answered_list.append(num_words - num_guesses)
@@ -101,13 +104,15 @@ def main():
         else:
             accuracy_list.append(0)
 
-    targetFile = "data/analysis.csv"
-    print(F"\nWriting to file {targetFile}...")
+    target_file = "data/analysis.csv"
+    print(F"\nWriting to file {target_file}...")
 
-    df = pd.DataFrame({"model name": modelName_list, "size": model_size_list, "correct labels": num_correct_list, "answered": num_answered_list, "accuracy": accuracy_list})
-    df.to_csv(targetFile, index=False)
+    df = pd.DataFrame({"model name": model_name_list, "size": model_size_list,
+                      "correct labels": num_correct_list, "answered": num_answered_list, "accuracy": accuracy_list})
+    df.to_csv(target_file, index=False)
 
-    print(F"Wrote to file {targetFile}")
+    print(F"Wrote to file {target_file}")
+
 
 if __name__ == "__main__":
     main()
